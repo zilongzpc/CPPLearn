1524ec8615c486e685dbf9c9ac7bed82540404e2
jared <jared@doogga.com>
2014-08-08 11:00:52 +0800

    Initial Commit

---
+++ xcode/OOP/OOP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OOP
+//
+//  Created by jared on 14-8-8.
+//  Copyright (c) 2014年 jared. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
